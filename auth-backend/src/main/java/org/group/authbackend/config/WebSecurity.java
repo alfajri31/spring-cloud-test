a8f2f626715f9fea1549c454c9fadc6e95614dac
Alfajri <alfajri607@gmail.com>
2024-12-16 02:58:07 +0700

    update

--- auth-backend/src/main/java/org/group/authbackend/config/WebSecurity.java
+++ auth-backend/src/main/java/org/group/authbackend/config/WebSecurity.java
@@ -10,11 +10,9 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
